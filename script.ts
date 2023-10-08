9d8b00ea5f81e1fa0a7817ab90d6bb53a9199d84
artёm rudoy <art.rudy@gmail.com>
2023-10-08 22:37:46 +0300

    player control added

--- script.ts
+++ script.ts
@@ -138,12 +138,103 @@ const player = {
   dy: 0,
 };
 
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function newPosition() {
+  player.x += player.dx;
+  player.y += player.dy;
+
+  detectWalls();
+}
+
+function detectWalls() {
+  //Left wall
+
+  if (player.x < 0) {
+    player.x = 0;
+  }
+
+  //Right wall
+  if (player.x > canvas.width - player.w) {
+    player.x = canvas.width - player.w;
+  }
+
+  //Ceiling
+  if (player.y < 0) {
+    player.y = 0;
+  }
+
+  //Floor
+
+  if (player.y > canvas.height - player.h) {
+    player.y = canvas.height - player.h;
+  }
+}
+
 function drawPlayer() {
   ctx.drawImage(image, player.x, player.y, player.w, player.h);
 }
 
 function update() {
+  clear();
+
   drawPlayer();
+
+  newPosition();
+
+  requestAnimationFrame(update);
+}
+
+function moveUp() {
+  player.dy = -player.speed;
+}
+
+function moveDown() {
+  player.dy = player.speed;
+}
+
+function moveRight() {
+  player.dx = player.speed;
+}
+
+function moveLeft() {
+  player.dx = -player.speed;
+}
+
+function keyDown(e) {
+  if (e.key === "ArrowRight" || e.key === "Right") {
+    moveRight();
+  } else if (e.key === "ArrowLeft" || e.key === "Left") {
+    moveLeft();
+  } else if (e.key === "ArrowUp" || e.key === "Up") {
+    moveUp();
+  } else if (e.key === "ArrowDown" || e.key === "Down") {
+    moveDown();
+  }
+
+  console.log(e.key);
+  e.stopPropagation();
+}
+
+function keyUp(e) {
+  if (
+    e.key == "Right" ||
+    e.key == "ArrowRight" ||
+    e.key == "Left" ||
+    e.key == "ArrowLeft" ||
+    e.key == "Up" ||
+    e.key == "ArrowUp" ||
+    e.key == "Down" ||
+    e.key == "ArrowDown"
+  ) {
+    player.dx = 0;
+    player.dy = 0;
+  }
 }
 
 update();
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
